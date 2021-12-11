357d9c42eca2d58c0dfb2a753a49e278d40998a1
MichellS3 <salcanshirley1b@gmail.com>
2021-12-10 21:07:36 -0500

    Deber01 literal a enviado desde GitBash

--- Deber01A.sh
+++ Deber01A.sh
@@ -6,11 +6,13 @@ cd unix/data/Saavedra2013/
 
 #Escribir el nombre del documento
 echo "Nombre del documento: " n1.txt
+
 #FILAS
 
 #Escribir el texto para diferenciar
 echo "Número de filas: "
 #Contabilizar las filas
+#cut va a cortar unicamente el valor numérico de la acción wc -l
 wc -l n1.txt |cut -d " " -f 1 $1
 
 #COLUMNAS
@@ -18,7 +20,7 @@ wc -l n1.txt |cut -d " " -f 1 $1
 #Escribir el texto para diferenciar
 echo "Número de columnas: "
 #Tomar la primera linea del archivo
-#Contabilizar los espacios, sabiendo que el nĆŗmero de espacios es igual al numero de caracteres en la fila, porque al final de la fila hay un espacio
+#Contabilizar los espacios, sabiendo que el número de espacios es igual al numero de caracteres en la fila, porque al final de la fila hay un espacio
 #grep -o toma y ordena los caracteres en filas
 # contabilizar las filas
 head -n 1 n1.txt | grep -o " " | wc -l $2
